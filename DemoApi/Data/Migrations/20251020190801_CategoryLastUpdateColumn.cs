135749faebfa82f351925aab33e0149819f1061f
Manuel Lopez <lopez.ledezma.manuel@gmail.com>
2025-10-21 19:50:03 -0600

    LastUpdate column was added to the Category model.

---
+++ DemoApi/Data/Migrations/20251020190801_CategoryLastUpdateColumn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryLastUpdateColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdateDate",
+                table: "Categories",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpdateDate",
+                table: "Categories");
+        }
+    }
+}
